7e0bf0a3daeb20262115eb86bc645df67e7e70fa
Yarden Porat <yardenporat@gmail.com>
2021-02-06 12:30:10 +0200

    optimized css

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,49 @@
 const path = require('path');
 const BrowserExtensionPlugin = require('extension-build-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'production',
-    entry: './project/devContentScript.js', //source js file
-    output: {
-        path: path.resolve(__dirname, 'dist'), //output folder
-        filename: 'contentScript.js', //output file
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: 'babel-loader',
-            },
+  mode: 'production',
+  entry: {
+    main: './project/devContentScript.js', //main js file
+    'popup-css': './project/popup.css',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'), //output folder
+    filename: '[name].js', //output file
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          // fallback to style-loader in development
+          process.env.NODE_ENV !== 'production'
+            ? 'style-loader'
+            : MiniCssExtractPlugin.loader,
+          'css-loader',
         ],
-    },
-    plugins: [
-        new BrowserExtensionPlugin({
-            devMode: false,
-            name: 'yad2-marker.zip',
-            directory: 'zips',
-            updateType: 'minor',
-        }),
-        new HtmlWebpackPlugin({
-            template: './project/popup.html',
-            filename: 'popup.html',
-        }),
+      },
     ],
+  },
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new BrowserExtensionPlugin({
+      devMode: false,
+      name: 'yad2-marker.zip',
+      directory: 'zips',
+      updateType: 'minor',
+    }),
+    new HtmlWebpackPlugin({
+      template: './project/popup.html',
+      filename: 'popup.html',
+    }),
+  ],
 };
