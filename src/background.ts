2690ab3fd7a0f6f27f34bcbc24df2a7c4d42ba8d
Yarden Porat <yardenporat@gmail.com>
2022-05-01 10:10:17 +0300

    fix: add url protection

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,7 @@
+const url = 'yad2.co.il';
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
-    if (changeInfo?.url) {
+    if (changeInfo?.url && changeInfo.url.includes(url)) {
         chrome.tabs.sendMessage(tabId, {
             message: 'urlChanged',
             url: changeInfo.url,
